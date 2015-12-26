dfd55f70fe0d5f6d3679bbd6b0b104c7125a46fe
Damien Pollet <damien.pollet@gmail.com>
2015-12-26 18:30:50 +0100

    Pick from one-element queue

--- src/random_queue.rs
+++ src/random_queue.rs
@@ -7,7 +7,7 @@ trait RandomQueue<T> {
 impl<T> RandomQueue<T> for Vec<T> {
     fn pick<R: Rng>(&mut self, rng: &mut R) -> Option<T> {
         if self.is_empty() { return None }
-        let picked = rng.gen_range(0, self.len() - 1);
+        let picked = rng.gen_range(0, self.len());
         Some(self.swap_remove(picked))
     }
 }
@@ -17,4 +17,11 @@ fn test_pick_empty() {
     let mut rng = ::rand::thread_rng();
     let mut vec = Vec::<usize>::new();
     assert!(vec.pick(&mut rng) == None);
+}
+
+#[test]
+fn test_pick_one() {
+    let mut rng = ::rand::thread_rng();
+    let mut vec = vec![1];
+    assert!(vec.pick(&mut rng) == Some(1));
 }
