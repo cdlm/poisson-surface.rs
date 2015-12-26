91655ea9d555865d382d08009574c9bf3e012e18
Damien Pollet <damien.pollet@gmail.com>
2015-12-26 18:40:49 +0100

    Pick from several elements

--- src/random_queue.rs
+++ src/random_queue.rs
@@ -25,3 +25,14 @@ fn test_pick_one() {
     let mut vec = vec![1];
     assert!(vec.pick(&mut rng) == Some(1));
 }
+
+#[test]
+fn test_pick_many() {
+    let mut rng = ::rand::thread_rng();
+    let mut vec = vec![1,2,3];
+    if let Some(picked) = vec.pick(&mut rng) {
+        assert!(picked == 1 || picked == 2 || picked == 3);
+        assert!(vec.len() == 2);
+        assert!(!vec.contains(&picked));
+    } else { assert!(false); }
+}
