47e2460eabc9d0172d56e2a548f4ed61b9c8ebfb
Damien Pollet <damien.pollet@gmail.com>
2015-12-29 01:35:11 +0100

    Display FPS in the screen corner

--- examples/piston.rs
+++ examples/piston.rs
@@ -5,7 +5,11 @@ extern crate graphics;
 use graphics::*;
 
 extern crate piston_window;
-use piston_window::{PistonWindow, WindowSettings, OpenGL};
+use piston_window::{PistonWindow, WindowSettings, OpenGL, Glyphs};
+
+extern crate find_folder;
+extern crate fps_counter;
+use fps_counter::FPSCounter;
 
 extern crate poisson;
 use poisson::PoissonSurface;
@@ -13,34 +17,56 @@ use poisson::PoissonSurface;
 struct App {
     // interactive state of the demo
     poisson: PoissonSurface,
+    fps: FPSCounter,
+    glyphs: Glyphs,
 }
 
 impl App {
-    fn new(poisson: PoissonSurface) -> App {
-        App { poisson: poisson }
+    fn new(poisson: PoissonSurface, glyphs: Glyphs) -> App {
+        App {
+            poisson: poisson,
+            fps: FPSCounter::new(),
+            glyphs: glyphs,
+        }
+    }
+
+    fn fps(&mut self) -> String {
+        self.fps.tick().to_string()
     }
 
     fn input(&mut self, input: Input) {}
 
     fn update(&mut self, args: UpdateArgs) {
-        if let Some(point) = self.poisson.generate_point() {
-            self.poisson.insert(point);
+        for _ in 1..10 {
+            if let Some(point) = self.poisson.generate_point() {
+                self.poisson.insert(point);
+            }
         }
     }
 
     fn render(&mut self, args: RenderArgs, window: PistonWindow) {
         window.draw_2d(|ctx, gfx| {
             let red = [1.0, 0.0, 0.0, 1.0];
+            let black = [0.0, 0.0, 0.0, 1.0];
+            let white = [1.0, 1.0, 1.0, 1.0];
+
             let dot = Ellipse::new(red);
 
-            clear([0.0, 0.0, 0.0, 1.0], gfx);
+            clear(black, gfx);
             for p in self.poisson.points_iter() {
                 dot.draw(ellipse::circle(p.x, p.y, 2.0),
                          default_draw_state(),
                          ctx.transform,
                          gfx);
             }
-        })
+
+            text(white,
+                 12,
+                 &self.fps(),
+                 &mut self.glyphs,
+                 ctx.transform.trans(20.0, 20.0),
+                 gfx)
+        });
     }
 }
 
@@ -53,7 +79,15 @@ fn main() {
                                    .build()
                                    .unwrap();
 
-    let mut app = App::new(PoissonSurface::new());
+    let assets = find_folder::Search::ParentsThenKids(3, 3)
+                     .for_folder("assets")
+                     .unwrap();
+    println!("{:?}", assets);
+    let ref font = assets.join("FiraSans-Regular.otf");
+    let factory = window.factory.borrow().clone();
+    let glyphs = Glyphs::new(font, factory).unwrap();
+
+    let mut app = App::new(PoissonSurface::new(), glyphs);
     let seed = app.poisson.random_point();
     app.poisson.insert(seed);
 
